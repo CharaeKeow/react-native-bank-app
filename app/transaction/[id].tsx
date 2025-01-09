11f06dddfbb59eb4b85911c56685b2b52682e877
CharaeKeow <charae.ehsin@gmail.com>
2025-01-10 00:49:38 +0800

    feat: add transaction detail screen

--- app/transaction/[id].tsx
+++ app/transaction/[id].tsx
@@ -1,14 +1,36 @@
 import { useLocalSearchParams } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { getTransactionData } from '../../data/get-transaction-data';
 
 export default function TransactionDetailScreen() {
   const { id } = useLocalSearchParams();
 
+  // Fetch transaction by id
+  const transaction = getTransactionData().find((trx) => trx.id === id);
+
+  if (!transaction) {
+    return (
+      <View style={styles.container}>
+        <Text>Transaction not found!</Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text>Transaction detail</Text>
-      <Text>ID: {id}</Text>
+      <Text>Transaction ID: {transaction.id}</Text>
+      <Text>Amount: {transaction.amount}</Text>
+      <Text>Description: {transaction.description}</Text>
+      <Text>Status: {transaction.status}</Text>
+      {transaction.relatedParty ? (
+        <View>
+          <Text>Receiver Name: {transaction.relatedParty.name}</Text>
+          <Text>Receiver ID: {transaction.relatedParty.id}</Text>
+        </View>
+      ) : null}
+
       <StatusBar style="auto" />
     </View>
   );
